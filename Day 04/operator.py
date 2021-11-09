adec2ef021c0feade4495c9da44de0908983a166
vickymore2003 <vickymore2003@gmail.com>
2021-11-09 17:02:41 +0530

    04

---
+++ Day 04/operator.py
@@ -0,0 +1,34 @@
+a = 11
+b = 3
+
+add = a+b
+sub = a-b
+multiply = a*b
+divide = a/b
+mod = a % b
+exp = a**b
+flodiv = a//b
+
+print("++Arithmatic Operator++")
+print("addition is ", add)
+print("difference is ", sub)
+print("product is ", multiply)
+print("divisiom is ", divide)
+print("remainder is ", mod)
+print("exponantianl is ", exp)
+print("Floor div is ", flodiv)
+
+print("++Assignment Operator++")
+a += 3
+print(a)
+
+print("++Comparision Operator++")
+print(a < b)
+
+print("++Logocal Operator++")
+print(a > 12 and b < 2)
+
+x = ["banana", "apple", "orange"]
+y = ["banana", "apple", "orange"]
+# x=y   ##remove comment for another result
+print(x is not y)
